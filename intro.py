55e89b98bacf82c7e601c582691487d86bec266b
arpitparekh <arpitparekh9@gmail.com>
2025-02-03 18:53:11 +0530

    first commit

---
+++ intro.py
@@ -0,0 +1,37 @@
+# hello world
+
+print("hello world")
+print(12)
+print(13.45)
+print("d")
+
+a = 12
+
+# rules of naming variables
+ab12 = 34
+_rt = 4
+num_12 = 56
+
+a = 12
+a  = 13
+
+a = "Hello"
+
+print(a)
+
+# single line comment
+
+"""
+
+
+multi line comment
+"""
+
+
+# sum of two numbers
+meraNumber = 10
+teraNumber = 20
+
+# +
+
+print(meraNumber + teraNumber)
