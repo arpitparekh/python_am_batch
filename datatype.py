afdf0a26646f4a4e44ab0dddeff44b32d70c1cb2
arpitparekh <arpitparekh9@gmail.com>
2025-02-05 18:53:49 +0530

    operators and datatypes

---
+++ datatype.py
@@ -0,0 +1,76 @@
+# number type
+# int complex
+
+a = 12
+print(a)
+print(type(a))
+
+num = 4545465546546546546
+print(num)
+print(type(num))
+
+########################################
+
+val = 12 + 14j
+print(val)
+print(type(val))
+
+#####################################
+# float
+
+mera_num = 12.34
+print(mera_num)
+print(type(mera_num))
+
+###########################################
+# bool
+
+isLogin = True
+netAvailable = False
+
+print(type(isLogin))
+print(type(netAvailable))
+
+# sequencial datatype
+# list
+# mutable # ordered
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango","mango"]
+print(fruits)
+print(type(fruits))
+
+values = [12,"Bascom",12.34,True,12.34]
+print(values)
+print(type(values))
+
+# tuple
+# immutable # ordered # fixed size
+
+cars = ("Ford", "Volvo", "BMW","BMW")
+print(cars)
+print(type(cars))
+
+# set
+# mutable # unordered # no duplicate
+
+vegetables = {"tomato", "potato", "onion", "carrot","onion","onion","ladyfinger","binjal"}
+print(vegetables)
+print(type(vegetables))
+
+# dictionary
+# mutable # unordered # no duplicate
+# key - value pair
+
+person = {"name":"John","age":36,"country":"Norway","name":"Sumit"}
+print(person)
+print(type(person))
+
+
+# string datatype
+kuchbhi = "Padho"
+print(kuchbhi)
+print(type(kuchbhi))
+
+data = None  # nothing
+print(data)
+print(type(data))
---
+++ operators.py
@@ -0,0 +1,56 @@
+# operators are the symbols used to perform operations on variables and values
+# 1. Arithmetic operators
+# + - * / // % **
+
+n1 = 10
+n2  = 20
+n3 = n1 + n2
+print(n1+n2)
+# print(10+20)
+print(n1-n2)
+print(n1*n2)
+print(n1/n2)  # float
+
+num1 = 40
+num2 = 10
+print(num1/num2)
+
+# integer division
+# //
+
+print(num1//num2)
+
+k = 40
+j = 45
+print(k//j)
+
+
+
+# % (modulus / modulo)  # reminder
+q = 20
+w = 12
+
+print(q % w)
+
+print(1234 % 10)   # 4
+print(1234 // 10)  # 123
+
+# ** (exponent)
+print(10 ** 101)
+
+
+# relational operators
+# < > <= >= == !=
+# relational operator always gives boolean value
+
+v1 = 10
+v2 = 20
+v3 = v1<v2
+print(v3)
+print(type(v3))
+print(v1<v2)
+print(v1>v2)
+print(v1<=v2)
+print(v1>=v2)
+print(v1==v2)  # equality operator
+print(v1!=v2)
