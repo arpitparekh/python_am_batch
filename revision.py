abcd4bc0685ac47b85d04050aed8494be69434d0
arpitparekh <arpitparekh9@gmail.com>
2025-03-10 18:21:43 +0530

    revision

---
+++ revision.py
@@ -0,0 +1,44 @@
+# variables
+# datatypes
+# typecastin
+# list tuple and dictionary(key value pair)
+# operators
+# control flow (conditional statements)
+# loops (for, while)
+# loop inside loop (patterns)
+# functions
+def sum(a,b): return a+b
+print(sum(12,13))
+# oppc
+# class object
+class Student:
+  def __init__(self,name,age):
+    self.name = name
+    self.age = age
+  def display(self):
+    print(self.name,self.age)
+
+s = Student("suimt",23)
+s.display()
+
+# constrcutor
+# inheritance (types of inheritance)
+# polymorphism
+# modules
+import operator   # system modules
+print(operator.mul(12,13))
+
+import intro
+# file hanlding
+# threading
+# exception handling(try, except, finally)
+# regex
+# dattime  # date formatting
+# math
+import re  # pending
+
+import sys
+print(sys.version)
+print(sys.platform)
+import os
+
